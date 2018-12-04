faf3fd2af5e36087110e94091c5d2a29fad65a4d
Helen Toomik <helen.toomik@tretton37.com>
2018-12-04 21:44:11 +0100

    Day 4

---
+++ aoc2018/Code/Day04.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2018.Code
+{
+    class Day04
+    {
+        public static (int, int) Solve1(List<string> data)
+        {
+            var totalsByGuardAndMinute = Analyse(data);
+
+            var totalsByGuard = totalsByGuardAndMinute.Keys.ToDictionary(k => k, k => totalsByGuardAndMinute[k].Values.Sum());
+            var max = totalsByGuard.Max(kvp => kvp.Value);
+            var guardWithMax = totalsByGuard.Single(kvp => kvp.Value == max).Key;
+            var max2 = totalsByGuardAndMinute[guardWithMax].Max(kvp => kvp.Value);
+            var minuteWithMax = totalsByGuardAndMinute[guardWithMax].Single(kvp => kvp.Value == max2).Key;
+
+            return (guardWithMax, minuteWithMax);
+        }
+
+        public static (int, int) Solve2(List<string> data)
+        {
+            var totalsByGuardAndMinute = Analyse(data);
+
+            var max = totalsByGuardAndMinute.SelectMany(kvp => kvp.Value.Values).Max();
+            var guardWithMax = totalsByGuardAndMinute.Single(kvp => kvp.Value.ContainsValue(max));
+            var minuteWithMax = guardWithMax.Value.Single(kvp => kvp.Value == max).Key;
+
+            return (guardWithMax.Key, minuteWithMax);
+        }
+
+        private static Record Parse(string line, int previousId)
+        {
+            var s1 = line.Split("] ");
+            var date = DateTime.Parse(s1[0].Substring(1));
+            var eventType = GetEventType(s1[1]);
+
+            var id = previousId;
+            if (eventType == EventType.StartedShift)
+            {
+                id = int.Parse(s1[1].Split(" ")[1].Substring(1));
+            }
+
+            return new Record
+            {
+                GuardId = id,
+                Timestamp = date,
+                EventType = eventType,
+            };
+        }
+
+        private static Dictionary<int, Dictionary<int, int>> Analyse(List<string> data)
+        {
+            var records = data.Select(Parse).ToList();
+            var totalsByGuardAndMinute = new Dictionary<int, Dictionary<int, int>>();
+
+            records = records.OrderBy(r => r.Timestamp).ToList();
+
+            var currentGuardId = 0;
+            var fellAsleepAt = DateTime.MinValue;
+            foreach (var record in records)
+            {
+                switch (record.EventType)
+                {
+                    case EventType.StartedShift:
+                        currentGuardId = record.GuardId;
+                        break;
+                    case EventType.FellAsleep:
+                        fellAsleepAt = record.Timestamp;
+                        break;
+                    case EventType.WokeUp:
+                        var currentTime = fellAsleepAt;
+                        if (!totalsByGuardAndMinute.ContainsKey(currentGuardId))
+                        {
+                            totalsByGuardAndMinute[currentGuardId] = new Dictionary<int, int>();
+                        }
+
+                        var totalsForGuardByMinute = totalsByGuardAndMinute[currentGuardId];
+                        while (currentTime < record.Timestamp)
+                        {
+                            var minute = currentTime.Minute;
+                            if (!totalsForGuardByMinute.ContainsKey(minute))
+                            {
+                                totalsForGuardByMinute[minute] = 0;
+                            }
+
+                            totalsForGuardByMinute[minute]++;
+                            currentTime = currentTime.AddMinutes(1);
+                        }
+
+                        break;
+                }
+            }
+
+            return totalsByGuardAndMinute;
+        }
+
+        private static EventType GetEventType(string s)
+        {
+            if (s.Contains("begins"))
+                return EventType.StartedShift;
+            if (s.Contains("asleep"))
+                return EventType.FellAsleep;
+            if (s.Contains("wakes"))
+                return EventType.WokeUp;
+            throw new Exception($"Cannot parse event type from '{s}'");
+        }
+
+        private struct Record
+        {
+            public int GuardId;
+            public DateTime Timestamp;
+            public EventType EventType;
+        }
+
+        private enum EventType
+        {
+            StartedShift,
+            FellAsleep,
+            WokeUp
+        }
+    }
+}
---
+++ aoc2018/Test/Test04.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test04
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test04(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var data = new List<string>
+            {
+                "[1518-11-01 00:00] Guard #10 begins shift",
+                "[1518-11-01 00:05] falls asleep",
+                "[1518-11-01 00:25] wakes up",
+                "[1518-11-01 00:30] falls asleep",
+                "[1518-11-01 00:55] wakes up",
+                "[1518-11-01 23:58] Guard #99 begins shift",
+                "[1518-11-02 00:40] falls asleep",
+                "[1518-11-02 00:50] wakes up",
+                "[1518-11-03 00:05] Guard #10 begins shift",
+                "[1518-11-03 00:24] falls asleep",
+                "[1518-11-03 00:29] wakes up",
+                "[1518-11-04 00:02] Guard #99 begins shift",
+                "[1518-11-04 00:36] falls asleep",
+                "[1518-11-04 00:46] wakes up",
+                "[1518-11-05 00:03] Guard #99 begins shift",
+                "[1518-11-05 00:45] falls asleep",
+                "[1518-11-05 00:55] wakes up",
+            };
+            var result = Day04.Solve1(data);
+            Assert.Equal(10, result.Item1);
+            Assert.Equal(24, result.Item2);
+        }
+        
+        [Fact]
+        public void Solve1()
+        {
+            var data = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input04.txt").ToList();
+            var result = Day04.Solve1(data);
+            var total = result.Item1 * result.Item2;
+            _output.WriteLine(total.ToString());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var data = new List<string>
+            {
+                "[1518-11-01 00:00] Guard #10 begins shift",
+                "[1518-11-01 00:05] falls asleep",
+                "[1518-11-01 00:25] wakes up",
+                "[1518-11-01 00:30] falls asleep",
+                "[1518-11-01 00:55] wakes up",
+                "[1518-11-01 23:58] Guard #99 begins shift",
+                "[1518-11-02 00:40] falls asleep",
+                "[1518-11-02 00:50] wakes up",
+                "[1518-11-03 00:05] Guard #10 begins shift",
+                "[1518-11-03 00:24] falls asleep",
+                "[1518-11-03 00:29] wakes up",
+                "[1518-11-04 00:02] Guard #99 begins shift",
+                "[1518-11-04 00:36] falls asleep",
+                "[1518-11-04 00:46] wakes up",
+                "[1518-11-05 00:03] Guard #99 begins shift",
+                "[1518-11-05 00:45] falls asleep",
+                "[1518-11-05 00:55] wakes up",
+            };
+            var result = Day04.Solve2(data);
+            Assert.Equal(99, result.Item1);
+            Assert.Equal(45, result.Item2);
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var data = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input04.txt").ToList();
+            var result = Day04.Solve2(data);
+            var total = result.Item1 * result.Item2;
+            _output.WriteLine(total.ToString());
+        }
+    }
+}
