c22a2ce00064bdb384c6dfd45205026e640eb027
Helen Toomik <helen.toomik@tretton37.com>
2018-12-08 12:58:15 +0100

    Day 7

---
+++ aoc2018/Code/Day07.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2018.Code
+{
+    class Day07
+    {
+        public static string Solve1(List<string> data)
+        {
+            var nodes = Parse(data, 0);
+
+            var order = string.Empty;
+            while (!nodes.All(n => n.Done))
+            {
+                var doable = nodes.Where(n => !n.Done && n.Doable);
+                var first = doable.OrderBy(n => n.Name).First();
+                order += first.Name;
+                first.Done = true;
+            }
+
+            return order;
+        }
+
+        public static (string, int) Solve2(List<string> data, int workers, int extraTime)
+        {
+            var nodes = Parse(data, extraTime);
+
+            var order = string.Empty;
+            var second = 0;
+
+            while (!nodes.All(n => n.Done))
+            {
+                var doing = nodes.Where(n => n.Doing);
+                foreach (var node in doing)
+                {
+                    if (node.DoneBy(second))
+                    {
+                        order += node.Name;
+                        node.Doing = false;
+                        node.Done = true;
+                    }
+                }
+
+                var doable = nodes.Where(n => !n.Done && !n.Doing && n.Doable).OrderBy(n => n.Name);
+                var busyWorkers = nodes.Count(n => n.Doing);
+                var freeWorkers = workers - busyWorkers;
+                var canDo = doable.Take(freeWorkers);
+
+                foreach (var node in canDo)
+                {
+                    node.StartAt(second);
+                }
+
+                second++;
+            }
+
+            return (order, second - 1);
+        }
+
+        private static List<Node> Parse(List<string> data, int extraTime)
+        {
+            var nodes = new Dictionary<char, Node>();
+            foreach (var line in data)
+            {
+                var n1 = line[5];
+                var n2 = line[36];
+
+                if (!nodes.ContainsKey(n1))
+                {
+                    nodes[n1] = new Node { Name = n1, ExtraTime = extraTime };
+                }
+                if (!nodes.ContainsKey(n2))
+                {
+                    nodes[n2] = new Node { Name = n2, ExtraTime = extraTime };
+                }
+
+                var node1 = nodes[n1];
+                var node2 = nodes[n2];
+
+                var edge = new Edge { DoFirst = node1, ThenDo = node2 };
+                node1.OutgoingEdges.Add(edge);
+                node2.IncomingEdges.Add(edge);
+            }
+
+            return nodes.Values.ToList();
+        }
+
+        private class Node
+        {
+            public char Name { get; set; }
+            public bool Doing { get; set; }
+            public bool Done { get; set; }
+            public int ExtraTime { get; set; }
+            private int StartedAt { get; set; }
+            private int TimeTaken => Name - 64 + ExtraTime;
+
+            public readonly List<Edge> IncomingEdges = new List<Edge>();
+            public readonly List<Edge> OutgoingEdges = new List<Edge>();
+
+            public bool Doable 
+            {
+                get { return IncomingEdges.All(edge => edge.DoFirst.Done); }
+            }
+
+            public void StartAt(int second)
+            {
+                Doing = true;
+                StartedAt = second;
+            }
+
+            public bool DoneBy(int second)
+            {
+                return second - StartedAt >= TimeTaken;
+            }
+        }
+
+        private class Edge
+        {
+            public Node DoFirst { get; set; }
+            public Node ThenDo { get; set; }
+        }
+    }
+}
---
+++ aoc2018/Test/Test07.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test07
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test07(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var data = new List<string>
+            {
+                "Step C must be finished before step A can begin.",
+                "Step C must be finished before step F can begin.",
+                "Step A must be finished before step B can begin.",
+                "Step A must be finished before step D can begin.",
+                "Step B must be finished before step E can begin.",
+                "Step D must be finished before step E can begin.",
+                "Step F must be finished before step E can begin.",
+            };
+            var result = Day07.Solve1(data);
+            Assert.Equal("CABDFE", result);
+        }
+        
+        [Fact]
+        public void Solve1()
+        {
+            Trace.Listeners.Add(new DefaultTraceListener());
+            var data = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input07.txt").ToList();
+            var result = Day07.Solve1(data);
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var data = new List<string>
+            {
+                "Step C must be finished before step A can begin.",
+                "Step C must be finished before step F can begin.",
+                "Step A must be finished before step B can begin.",
+                "Step A must be finished before step D can begin.",
+                "Step B must be finished before step E can begin.",
+                "Step D must be finished before step E can begin.",
+                "Step F must be finished before step E can begin.",
+            };
+            var result = Day07.Solve2(data, 2, 0);
+            Assert.Equal("CABFDE", result.Item1);
+            Assert.Equal(15, result.Item2);
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var data = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input07.txt").ToList();
+            var result = Day07.Solve2(data, 5, 60);
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
