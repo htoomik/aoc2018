0d287b9c9bc39aad22238fbb0a174586346fc5f4
Helen Toomik <helen.toomik@tretton37.com>
2018-12-08 15:17:08 +0100

    Day 8

---
+++ aoc2018/Code/Day08.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2018.Code
+{
+    class Day08
+    {
+        public int Solve1(string data)
+        {
+            var nums = data.Split(" ").Select(int.Parse).ToList();
+            var nodes = new List<Node>();
+
+            Parse(nodes, nums, 0);
+            
+            return nodes.Sum(n => n.Metadata.Sum());
+        }
+
+        public int Solve2(string data)
+        {
+            var nums = data.Split(" ").Select(int.Parse).ToList();
+            var nodes = new List<Node>();
+
+            var root = Parse(nodes, nums, 0);
+            var value = root.GetValue();
+
+            return value;
+        }
+
+        private Node Parse(List<Node> nodes, List<int> nums, int pos)
+        {
+            var childCount = nums[pos];
+            var metadataCount = nums[pos + 1];
+
+            var node = new Node();
+            nodes.Add(node);
+
+            var nextChildStartsAt = pos + 2;
+
+            for (var i = 0; i < childCount; i++)
+            {
+                var child = Parse(nodes, nums, nextChildStartsAt);
+                node.Children.Add(child);
+                nextChildStartsAt += child.Length;
+            }
+
+            for (var i = 0; i < metadataCount; i++)
+            {
+                node.Metadata.Add(nums[nextChildStartsAt + i]);
+            }
+
+            return node;
+        }
+
+        private class Node
+        {
+            public List<Node> Children { get; } = new List<Node>();
+            public List<int> Metadata { get; } = new List<int>();
+
+            public int Length => 2 + Children.Sum(c => c.Length) + Metadata.Count;
+
+            public int GetValue()
+            {
+                var sum = 0;
+                if (Children.Count == 0)
+                {
+                    sum = Metadata.Sum();
+                }
+                else
+                {
+                    foreach (var md in Metadata)
+                    {
+                        if (md <= Children.Count)
+                        {
+                            sum += Children[md - 1].GetValue();
+                        }
+                    }
+                }
+
+                return sum;
+            }
+        }
+    }
+}
---
+++ aoc2018/Test/Test08.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.IO;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test08
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test08(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var data = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+            var result = new Day08().Solve1(data);
+            Assert.Equal(138, result);
+        }
+        
+        [Fact]
+        public void Solve1()
+        {
+            Trace.Listeners.Add(new DefaultTraceListener());
+            var data = File.ReadAllText("C:\\Code\\aoc2018\\aoc2018\\Data\\input08.txt");
+            var result = new Day08().Solve1(data);
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var data = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+            var result = new Day08().Solve2(data);
+            Assert.Equal(66, result);
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var data = File.ReadAllText("C:\\Code\\aoc2018\\aoc2018\\Data\\input08.txt");
+            var result = new Day08().Solve2(data);
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
