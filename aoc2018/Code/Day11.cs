658ff5b4caec0530e9933e89a1892e5b90d3221f
Helen Toomik <helen.toomik@tretton37.com>
2018-12-11 22:55:21 +0100

    Day 11

---
+++ aoc2018/Code/Day11.cs
@@ -0,0 +1,98 @@
+﻿namespace aoc2018.Code
+{
+    class Day11
+    {
+        private const int GridSize = 300;
+
+        public static Cell Solve1(int serial)
+        {
+            var grid = GenerateGrid(serial);
+            var max = FindMax(grid, 3);
+            return max;
+        }
+
+        public static (Cell cell, int size) Solve2(int serial)
+        {
+            var grid = GenerateGrid(serial);
+            Cell bestResult = null;
+            var bestSize = 0;
+
+            for (var size = 1; size <= 300; size++)
+            {
+                var cell = FindMax(grid, size);
+                if (bestResult == null || cell.Level > bestResult.Level)
+                {
+                    bestResult = cell;
+                    bestSize = size;
+                }
+
+                // result plotted against size has a bellish shape; once it's down to zero we can quit
+                if (cell.Level == 0 && size > 10)
+                {
+                    break;
+                }
+            }
+            return (bestResult, bestSize);
+        }
+
+        public static int GetPowerLevel(int x, int y, int serial)
+        {
+            var rackId = x + 10;
+            var step = (rackId * y + serial) * rackId;
+            var hundreds = step / 100 % 10;
+            var powerLevel = hundreds - 5;
+            return powerLevel;
+        }
+
+        private static int[,] GenerateGrid(int serial)
+        {
+            var grid = new int[GridSize + 1, GridSize + 1];
+            for (var x = 1; x <= GridSize; x++)
+            {
+                for (var y = 1; y <= GridSize; y++)
+                {
+                    grid[x, y] = GetPowerLevel(x, y, serial);
+                }
+            }
+
+            return grid;
+        }
+
+        private static Cell FindMax(int[,] grid, int size)
+        {
+            var bestX = 0;
+            var bestY = 0;
+            var max = 0;
+            for (var i = 1; i <= GridSize - size; i++)
+            {
+                for (var j = 1; j <= GridSize - size; j++)
+                {
+                    var level = 0;
+                    for (int k = 0; k < size; k++)
+                    {
+                        for (int m = 0; m < size; m++)
+                        {
+                            level += grid[i + k, j + m];
+                        }
+                    }
+
+                    if (level > max)
+                    {
+                        bestX = i;
+                        bestY = j;
+                        max = level;
+                    }
+                }    
+            }
+
+            return new Cell { X = bestX, Y = bestY, Level = max };
+        }
+
+        public class Cell
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Level { get; set; }
+        }
+    }
+}
---
+++ aoc2018/Test/Test11.cs
@@ -0,0 +1,65 @@
+﻿using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test11
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test11(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData(3, 5, 8, 4)]
+        [InlineData(122, 79, 57, -5)]
+        [InlineData(217, 196, 39, 0)]
+        [InlineData(101, 153, 71, 4)]
+        public void TestPowerLevel(int x, int y, int serial, int expected)
+        {
+            var result = Day11.GetPowerLevel(x, y, serial);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(18, 33, 45, 29)]
+        [InlineData(42, 21, 61, 30)]
+        public void Test1(int serial, int expectedX, int expectedY, int expectedMax)
+        {
+            var result = Day11.Solve1(serial);
+            Assert.Equal(expectedX, result.X);
+            Assert.Equal(expectedY, result.Y);
+            Assert.Equal(expectedMax, result.Level);
+        }
+        
+        [Theory]
+        [InlineData(18, 90, 269, 16, 113)]
+        [InlineData(42, 232, 251, 12, 119)]
+        public void Test2(int serial, int expectedX, int expectedY, int expectedSize, int expectedMax)
+        {
+            var (cell, size) = Day11.Solve2(serial);
+            Assert.Equal(expectedX, cell.X);
+            Assert.Equal(expectedY, cell.Y);
+            Assert.Equal(expectedMax, cell.Level);
+            Assert.Equal(expectedSize, size);
+        }
+        
+        [Fact]
+        public void Solve1()
+        {
+            var result = Day11.Solve1(4151);
+            _output.WriteLine(result.ToString());
+        }
+        
+        [Fact]
+        public void Solve2()
+        {
+            var (cell, size) = Day11.Solve2(4151);
+            _output.WriteLine($"X: {cell.X}, Y: {cell.Y}, Level: {cell.Level}, Size: {size}");
+            _output.WriteLine($"{cell.X},{cell.Y},{size}");
+        }
+    }
+}
