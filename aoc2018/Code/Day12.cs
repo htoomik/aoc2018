58bf4535c305a744aaadce95168c84f9465f5e14
Helen Toomik <helen.toomik@tretton37.com>
2018-12-14 19:29:15 +0100

    Day 12 part 1

---
+++ aoc2018/Code/Day12.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2018.Code
+{
+    class Day12
+    {
+        public static int Solve1(List<string> input, long generations)
+        {
+            var (plants, rules) = Parse(input);
+
+            for (var i = 0; i < generations; i++)
+            {
+                plants = Spread(plants, rules);
+            }
+
+            var sum = 0;
+            foreach (var plant in plants)
+            {
+                if (plant.Value)
+                    sum += plant.Key;
+            }
+
+            return sum;
+        }
+
+        private static (Dictionary<int, bool>, List<Rule>) Parse(List<string> input)
+        {
+            var initialState = new Dictionary<int, bool>();
+
+            var stateLine = input[0];
+            var state = stateLine.Split(": ")[1];
+            for (var i = 0; i < state.Length; i++)
+            {
+                initialState[i] = state[i] == '#';
+            }
+
+            var ruleLines = input.Skip(2);
+            var rules = ruleLines.Select(ParseRule).ToList();
+
+            return (initialState, rules);
+        }
+
+        public static Rule ParseRule(string input)
+        {
+            var parts = input.Split(" => ");
+
+            var condition = new bool[5];
+            var key = 0;
+            for (var i = 0; i < 5; i++)
+            {
+                var b = parts[0][i] == '#';
+                condition[i] = b;
+                key = key  | (b ? 1 << i : 0);
+            }
+
+            var outcome = parts[1] == "#";
+
+            return new Rule { Outcome = outcome, Key = key };
+        }
+
+        private static Dictionary<int, bool> Spread(Dictionary<int, bool> plants, List<Rule> rules)
+        {
+            var newPlants = new Dictionary<int, bool>();
+            var minIndex = plants.Keys.Min() - 2;
+            var maxIndex = plants.Keys.Max() + 2;
+            for (var i = minIndex; i <= maxIndex; i++)
+            {
+                var state = GetState(plants, i);
+                var matchingRule = FindRule(state, rules);
+                if (matchingRule != null && matchingRule.Value.Outcome)
+                {
+                    newPlants[i] = true;
+                }
+            }
+
+            return newPlants;
+        }
+
+        private static Rule? FindRule(int state, List<Rule> rules)
+        {
+            return rules.SingleOrDefault(r => r.Key == state);
+        }
+
+        public static int GetState(Dictionary<int, bool> plants, int i)
+        {
+            var state = 0;
+            for (var j = 0; j < 5; j++)
+            {
+                var b = plants.ContainsKey(i + j - 2) && plants[i + j - 2];
+                var flag = b ? 1 << j : 0;
+                state = state | flag;
+            }
+            return state;
+        }
+
+        public struct Rule
+        {
+            public int Key { get; set; }
+            public bool Outcome { get; set; }
+        }
+    }
+}
---
+++ aoc2018/Test/Test12.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test12
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test12(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var input = @"initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #";
+            var list = input.Split("\r\n").ToList();
+            var result = Day12.Solve1(list, 20);
+            Assert.Equal(325, result);
+        }
+
+        [Theory]
+        [InlineData("...## => #", 24, true)]
+        [InlineData("#.#.# => .", 21, false)]
+        public void RuleParsing(string input, int key, bool outcome)
+        {
+            var rule = Day12.ParseRule(input);
+            Assert.Equal(outcome, rule.Outcome);
+            Assert.Equal(key, rule.Key);
+        }
+
+        [Fact]
+        public void GetState()
+        {
+            // Plants in pots 1, 2, 4
+            // Looking at pot 3
+            // Expecting "##.#." or 11
+
+            var plants = new Dictionary<int, bool> { { 1, true }, { 2, true }, { 4, true } };
+            var state = Day12.GetState(plants, 3);
+            Assert.Equal(11, state);
+        }
+
+        [Fact]
+        public void Solve1()
+        {
+            var input = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input12.txt").ToList();
+            var result = Day12.Solve1(input, 20);
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var input = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input12.txt").ToList();
+            var result = Day12.Solve1(input, 50000000000);
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
