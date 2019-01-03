0403750131eaa65ae638e0eca46ee8f640b2cacb
Helen Toomik <helen.toomik@tretton37.com>
2019-01-03 10:30:57 +0100

    Day 18

---
+++ aoc2018/Code/Day18.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace aoc2018.Code
+{
+    public class Day18
+    {
+        public static (string state, int answer, int repeat) Solve(string input, int minutes)
+        {
+            var array = Parse(input);
+            var i1000 = "";
+            var repeat = 0;
+
+            for (var i = 0; i < minutes; i++)
+            {
+                array = Mutate(array);
+
+                if (i == 1000)
+                {
+                    i1000 = Print(array);
+                }
+
+                if (i > 1000 && repeat == 0)
+                {
+                    var temp = Print(array);
+                    if (temp == i1000)
+                    {
+                        repeat = i;
+                    }
+                }
+            }
+
+            var state = Print(array);
+            var value = Evaluate(array);
+            return (state, value, repeat);
+        }
+
+        private static char[,] Parse(string input)
+        {
+            var lines = input.Trim().Split("\n").Select(l => l.Trim()).ToArray();
+            var array = new char[lines.Length, lines[0].Length];
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                for (var j = 0; j < line.Length; j++)
+                {
+                    array[i, j] = line[j];
+                }
+            }
+
+            return array;
+        }
+
+        private static char[,] Mutate(char[,] array)
+        {
+            var newArray = new char[array.GetLength(0), array.GetLength(1)];
+
+            // Skip edges to begin with
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    var currentValue = array[i, j];
+                    var trees = CountSurrounding(array, i, j, '|');
+                    var yards = CountSurrounding(array, i, j, '#');
+                    char newValue;
+
+                    switch (currentValue)
+                    {
+                        case '.':
+                        {
+                            newValue = trees >= 3 ? '|' : '.';
+                            break;
+                        }
+                        case '|':
+                        {
+                            newValue = yards >= 3 ? '#' : '|';
+                            break;
+                        }
+                        case '#':
+                        {
+                            newValue = yards >= 1 && trees >= 1 ? '#' : '.';
+                            break;
+                        }
+                        default:
+                            throw new Exception("Unexpected character");
+                    }
+
+                    newArray[i, j] = newValue;
+                }
+            }
+
+            return newArray;
+        }
+
+        private static int CountSurrounding(char[,] array, int i, int j, char c)
+        {
+            var count = 0;
+            for (var k = -1; k < 2; k++)
+            {
+                for (var m = -1; m < 2; m++)
+                {
+                    if (k == 0 && m == 0)
+                        continue;
+
+                    if (i + k < 0 || i + k >= array.GetLength(0))
+                        continue;
+
+                    if (j + m < 0 || j + m >= array.GetLength(1))
+                        continue;
+
+                    if (array[i + k, j + m] == c)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static string Print(char[,] array)
+        {
+            var s = new StringBuilder();
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    s.Append(array[i, j]);
+                }
+
+                s.AppendLine();
+            }
+
+            return s.ToString().Trim();
+        }
+
+        private static int Evaluate(char[,] array)
+        {
+            var woods = 0;
+            var yards = 0;
+
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    var c = array[i, j];
+
+                    if (c == '|')
+                        woods++;
+                    if (c == '#')
+                        yards++;
+                }
+            }
+
+            return woods * yards;
+        }
+    }
+}
---
+++ aoc2018/Test/Test18.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test18
+    {
+        private readonly ITestOutputHelper _output;
+
+        private const string Input = @"
+.#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|.";
+
+        private const string Expected1 = @"
+.......##.
+......|###
+.|..|...#.
+..|#||...#
+..##||.|#|
+...#||||..
+||...|||..
+|||||.||.|
+||||||||||
+....||..|.";
+
+        private const string Expected10 = @"
+.||##.....
+||###.....
+||##......
+|##.....##
+|##.....##
+|##....##|
+||##.####|
+||#####|||
+||||#|||||
+||||||||||";
+
+        public Test18(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var result = Day18.Solve(Input, 1);
+            Assert.Equal(Expected1.Trim(), result.state);
+        }
+
+        [Fact]
+        public void Test10()
+        {
+            var result = Day18.Solve(Input, 10);
+            Assert.Equal(Expected10.Trim(), result.state);
+            Assert.Equal(1147, result.answer);
+        }
+
+        [Fact]
+        public void Solve()
+        {
+            var input = File.ReadAllText("C:\\Code\\aoc2018\\aoc2018\\Data\\input18.txt");
+            var result = Day18.Solve(input, 10);
+            _output.WriteLine(result.answer.ToString());
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var input = File.ReadAllText("C:\\Code\\aoc2018\\aoc2018\\Data\\input18.txt");
+            var result = Day18.Solve(input, 1100);
+            _output.WriteLine(result.answer.ToString());
+            _output.WriteLine(result.repeat.ToString());
+
+            var cycle = result.repeat - 1000;
+            var remainder = (1000000000 - 1000) % cycle;
+            var finalResult = Day18.Solve(input, 1000 + remainder);
+            _output.WriteLine(finalResult.answer.ToString());
+        }
+    }
+}
