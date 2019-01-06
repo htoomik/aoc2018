6800f5b0e218de4182821f64b136b970e7027fff
Helen Toomik <helen.toomik@tretton37.com>
2019-01-06 15:46:22 +0100

    Day 24 part 1

---
+++ aoc2018/Code/Day24.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace aoc2018.Code
+{
+    class Day24
+    {
+        public static int Solve(string input)
+        {
+            var (imm, inf) = Parse(input);
+
+            while (imm.Count > 0 && inf.Count > 0)
+            {
+                var allTargets = PairUp(imm, inf);
+
+                foreach (var pair in allTargets)
+                {
+                    Attack(pair.Item1, pair.Item2);
+                }
+
+                imm = imm.Where(g => g.UnitCount > 0).ToList();
+                inf = inf.Where(g => g.UnitCount > 0).ToList();
+            }
+
+            var winningArmy = imm.Count > 0 ? imm : inf;
+            return winningArmy.Sum(g => g.UnitCount);
+        }
+
+        public static IEnumerable<Tuple<Group, Group>> PairUp(List<Group> imm, List<Group> inf)
+        {
+            var immTargets = SelectTargets(imm, inf);
+            var infTargets = SelectTargets(inf, imm);
+
+            return infTargets
+                .Union(immTargets)
+                .OrderByDescending(kvp => kvp.Key.Initiative)
+                .Select(kvp => new Tuple<Group, Group>(kvp.Key, kvp.Value));
+        }
+
+        public static (List<Group> imm, List<Group> inf) Parse(string input)
+        {
+            var armies = input.Replace("\r\n", "\n").Split("\n\n");
+
+            var imm = ParseArmy(armies[0], "Immune system");
+            var inf = ParseArmy(armies[1], "Infection");
+
+            return (imm, inf);
+        }
+
+        public static List<Group> ParseArmy(string army, string name)
+        {
+            var imm = new List<Group>();
+            var lines = army.Trim().Split("\n");
+            var i = 0;
+
+            // First line contains the army's name - skip
+            foreach (var line in lines.Skip(1))
+            {
+                i++;
+                var regex = new Regex(@"(\d+) units each with (\d+) hit points (\((.*)\) )?with an attack that does (\d+) (\w+) damage at initiative (\d+)");
+                var match = regex.Match(line.Trim());
+                
+                var unitCount = int.Parse(match.Groups[1].Value);
+                var hitPoints = int.Parse(match.Groups[2].Value);
+                var attackDamage = int.Parse(match.Groups[5].Value);
+                var initiative = int.Parse(match.Groups[7].Value);
+
+                var attackType = ParseAttackType(match.Groups[6].Value);
+
+                var group = new Group
+                {
+                    Name =  name + " " + i,
+                    UnitCount = unitCount,
+                    HitPoints = hitPoints,
+                    AttackDamage = attackDamage,
+                    Initiative = initiative,
+                    AttackType = attackType,
+                };
+
+                // Immunities and weaknesses are hard to regex.
+                // One, both or neither may be present, and the order is unpredictable.
+                var immunitiesAndWeaknesses = match.Groups[4].Value;
+                var parts = immunitiesAndWeaknesses.Split("; ");
+                var immunityPart = parts.SingleOrDefault(p => p.Contains("immune to"));
+                var weaknessPart = parts.SingleOrDefault(p => p.Contains("weak to"));
+
+                if (immunityPart != null)
+                {
+                    group.Immunities = immunityPart.Replace("immune to ", "").Split(", ").Select(ParseAttackType).Aggregate((at, at2) => at | at2);
+                }
+
+                if (weaknessPart != null)
+                {
+                    group.Weaknesses = weaknessPart.Replace("weak to ", "").Split(", ").Select(ParseAttackType).Aggregate((at, at2) => at | at2);
+                }
+
+                imm.Add(group);
+            }
+
+            return imm;
+        }
+
+        private static AttackType ParseAttackType(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return AttackType.None;
+
+            var s = value.Substring(0, 1).ToUpper() + value.Substring(1);
+            var attackType = Enum.Parse<AttackType>(s);
+            return attackType;
+        }
+
+        public static Dictionary<Group, Group> SelectTargets(List<Group> attackers, List<Group> defenders)
+        {
+            var sortedAttackers = attackers.OrderByDescending(g => g.EffectivePower).ThenByDescending(g => g.Initiative);
+            var targets = new Dictionary<Group, Group>();
+            var availableDefenders = new List<Group>();
+            availableDefenders.AddRange(defenders);
+
+            foreach (var attacker in sortedAttackers)
+            {
+                var damagePerDefender = new Dictionary<Group, int>();
+                foreach (var defender in availableDefenders)
+                {
+                    var damage = DamageIfAttacked(attacker, defender);
+                    damagePerDefender[defender] = damage;
+                }
+
+                var ordered = damagePerDefender
+                    .OrderByDescending(kvp => kvp.Value)
+                    .ThenByDescending(kvp => kvp.Key.EffectivePower)
+                    .ThenByDescending(kvp => kvp.Key.Initiative);
+                var bestTarget = ordered.FirstOrDefault();
+                if (bestTarget.Value > 0)
+                {
+                    targets[attacker] = bestTarget.Key;
+                    availableDefenders.Remove(bestTarget.Key);
+                }
+            }
+
+            return targets;
+        }
+
+        public static int DamageIfAttacked(Group attacker, Group defender)
+        {
+            if (defender.Immunities.HasFlag(attacker.AttackType))
+            {
+                return 0;
+            }
+
+            if (defender.Weaknesses.HasFlag(attacker.AttackType))
+            {
+                return attacker.EffectivePower * 2;
+            }
+
+            return attacker.EffectivePower;
+        }
+
+        public static void Attack(Group attacker, Group defender)
+        {
+            if (attacker.UnitCount <= 0)
+                return;
+
+            var damage = DamageIfAttacked(attacker, defender);
+            var unitsKilled = damage / defender.HitPoints;
+            defender.UnitCount -= unitsKilled;
+        }
+
+        [DebuggerDisplay("{Name}")]
+        public class Group
+        {
+            public string Name { get; set; }
+            public int UnitCount { get; set; }
+            public int HitPoints { get; set; }
+            public int AttackDamage { get; set; }
+            public int Initiative { get; set; }
+            public AttackType AttackType { get; set; }
+            public AttackType Weaknesses { get; set; }
+            public AttackType Immunities { get; set; }
+
+            public int EffectivePower => UnitCount * AttackDamage;
+        }
+
+        public enum Allegiance
+        {
+            ImmuneSystem,
+            Infection
+        }
+
+        [Flags]
+        public enum AttackType
+        {
+            None = 0,
+            Fire = 1,
+            Radiation = 2,
+            Bludgeoning = 4,
+            Slashing = 8,
+            Cold = 16
+        }
+    }
+}
---
+++ aoc2018/Test/Test24.cs
@@ -0,0 +1,138 @@
+﻿using System.IO;
+using System.Linq;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test24
+    {
+        private readonly ITestOutputHelper _output;
+        
+        public Test24(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private const string Input = @"
+Immune System:
+17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+
+Infection:
+801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4";
+
+        [Fact]
+        public void TestParse()
+        {
+            var (imm, inf) = Day24.Parse(Input.Trim());
+            Assert.Equal(2, imm.Count);
+            Assert.Equal(2, inf.Count);
+
+            Assert.Equal(17, imm[0].UnitCount);
+            Assert.Equal(5390, imm[0].HitPoints);
+            Assert.Equal(4507, imm[0].AttackDamage);
+            Assert.Equal(2, imm[0].Initiative);
+            Assert.Equal(Day24.AttackType.Fire, imm[0].AttackType);
+            Assert.Equal(Day24.AttackType.Radiation | Day24.AttackType.Bludgeoning, imm[0].Weaknesses);
+            Assert.Equal(Day24.AttackType.None, imm[0].Immunities);
+
+            Assert.Equal(989, imm[1].UnitCount);
+            Assert.Equal(1274, imm[1].HitPoints);
+            Assert.Equal(25, imm[1].AttackDamage);
+            Assert.Equal(3, imm[1].Initiative);
+            Assert.Equal(Day24.AttackType.Slashing, imm[1].AttackType);
+            Assert.Equal(Day24.AttackType.Slashing | Day24.AttackType.Bludgeoning, imm[1].Weaknesses);
+            Assert.Equal(Day24.AttackType.Fire, imm[1].Immunities);
+        }
+
+        [Theory]
+        [InlineData("1 units each with 1 hit points with an attack that does 1 cold damage at initiative 1")]
+        [InlineData("1 units each with 1 hit points (weak to radiation) with an attack that does 1 cold damage at initiative 1")]
+        [InlineData("1 units each with 1 hit points (immune to radiation) with an attack that does 1 cold damage at initiative 1")]
+        [InlineData("1 units each with 1 hit points (immune to radiation; weak to fire) with an attack that does 1 cold damage at initiative 1")]
+        [InlineData("1 units each with 1 hit points (weak to radiation; immune to fire) with an attack that does 1 cold damage at initiative 1")]
+        public void TestParseSpecials(string input)
+        {
+            Day24.ParseArmy(input, null);
+        }
+
+        [Fact]
+        public void TestDamageIfAttacked()
+        {
+            var (imm, inf) = Day24.Parse(Input.Trim());
+
+            var inf1AttacksImm1 = Day24.DamageIfAttacked(inf[0], imm[0]);
+            Assert.Equal(185832, inf1AttacksImm1);
+
+            var inf1AttacksImm2 = Day24.DamageIfAttacked(inf[0], imm[1]);
+            Assert.Equal(185832, inf1AttacksImm2);
+
+            var inf2AttacksImm2 = Day24.DamageIfAttacked(inf[1], imm[1]);
+            Assert.Equal(107640 , inf2AttacksImm2);
+
+            var imm1AttacksInf1 = Day24.DamageIfAttacked(imm[0], inf[0]);
+            Assert.Equal(76619, imm1AttacksInf1);
+
+            var imm1AttacksInf2 = Day24.DamageIfAttacked(imm[0], inf[1]);
+            Assert.Equal(153238, imm1AttacksInf2);
+
+            var imm2AttacksInf1 = Day24.DamageIfAttacked(imm[1], inf[0]);
+            Assert.Equal(24725, imm2AttacksInf1);
+        }
+
+        [Fact]
+        public void TestSelectTargets()
+        {
+            var (imm, inf) = Day24.Parse(Input.Trim());
+
+            var targets1 = Day24.SelectTargets(inf, imm);
+            Assert.Equal(targets1[inf[0]], imm[0]);
+            Assert.Equal(targets1[inf[1]], imm[1]);
+
+            var targets2 = Day24.SelectTargets(imm, inf);
+            Assert.Equal(targets2[imm[1]], inf[0]);
+            Assert.Equal(targets2[imm[0]], inf[1]);
+        }
+
+        [Fact]
+        public void TestPairUp()
+        {
+            var (imm, inf) = Day24.Parse(Input.Trim());
+
+            var targets = Day24.PairUp(imm, inf).ToList();
+
+            Assert.Equal(inf[1], targets[0].Item1);
+            Assert.Equal(imm[1], targets[0].Item2);
+
+            Assert.Equal(imm[1], targets[1].Item1);
+            Assert.Equal(inf[0], targets[1].Item2);
+        }
+
+        [Fact]
+        public void TestAttack()
+        {
+            var (imm, inf) = Day24.Parse(Input.Trim());
+
+            var targets1 = Day24.SelectTargets(inf, imm);
+            var targets2 = Day24.SelectTargets(imm, inf);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var result = Day24.Solve(Input);
+            Assert.Equal(5216, result);
+        }
+
+        [Fact]
+        public void Solve()
+        {
+            var input = File.ReadAllText("C:\\Code\\aoc2018\\aoc2018\\Data\\input24.txt");
+            var result = Day24.Solve(input);
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
