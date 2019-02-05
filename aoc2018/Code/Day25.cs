30c870c0b4f257e7d8853dd7e5f065e466ce2035
Helen Toomik <helen.toomik@tretton37.com>
2019-02-05 22:48:59 +0100

    Day 25 part 1

---
+++ aoc2018/Code/Day25.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2018.Code
+{
+    public static class Day25
+    {
+        public static int Solve(string input)
+        {
+            var points = Parse(input);
+
+            var constellations = new List<List<Point>>();
+
+            // First attempt
+            foreach (var point in points)
+            {
+                var connected = false;
+                foreach (var constellation in constellations)
+                {
+                    if (Connected(point, constellation))
+                    {
+                        constellation.Add(point);
+                        connected = true;
+                        break;
+                    }
+                }
+
+                if (!connected)
+                {
+                    constellations.Add(new List<Point> { point });
+                }
+            }
+
+            // Try and join up existing constellations
+            var changed = true;
+            while (changed)
+            {
+                changed = false;
+
+                for (int i = 0; i < constellations.Count; i++)
+                {
+                    for (int j = 0; j < constellations.Count; j++)
+                    {
+                        if (i == j)
+                            continue;
+
+                        foreach (var point1 in constellations[i])
+                        {
+                            if (Connected(point1, constellations[j]))
+                            {
+                                constellations[i].AddRange(constellations[j]);
+                                constellations.RemoveAt(j);
+                                changed = true;
+                                break;
+                            }
+                        }
+
+                        if (changed)
+                            break;
+                    }
+
+                    if (changed)
+                        break;
+                }
+            }
+
+            return constellations.Count;
+        }
+
+        private static List<Point> Parse(string input)
+        {
+            var lines = input.Trim().Split("\n");
+            return lines
+                .Select(line => line.Trim().Split(","))
+                .Select(parts => new Point
+                {
+                    X = parts[0].ToInt(),
+                    Y = parts[1].ToInt(),
+                    Z = parts[2].ToInt(),
+                    W = parts[3].ToInt(),
+                })
+                .ToList();
+        }
+
+        private static int ToInt(this string s)
+        {
+            return int.Parse(s.Trim());
+        }
+
+        private static bool Connected(Point point, List<Point> constellation)
+        {
+            return constellation.Any(p => Distance(p, point) <= 3);
+        }
+
+        private static int Distance(Point point1, Point point2)
+        {
+            return Math.Abs(point1.X - point2.X) +
+                   Math.Abs(point1.Y - point2.Y) +
+                   Math.Abs(point1.Z - point2.Z) +
+                   Math.Abs(point1.W - point2.W);
+        }
+
+        private struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+            public int W { get; set; }
+        }
+    }
+}
---
+++ aoc2018/Test/Test25.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using aoc2018.Code;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace aoc2018.Test
+{
+    public class Test25
+    {
+        private readonly ITestOutputHelper _output;
+        
+        public Test25(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private const string Input1 = @"
+ 0,0,0,0
+ 3,0,0,0
+ 0,3,0,0
+ 0,0,3,0
+ 0,0,0,3
+ 0,0,0,6
+ 9,0,0,0
+12,0,0,0";
+
+        private const string Input2 = @"
+-1,2,2,0
+0,0,2,-2
+0,0,0,-2
+-1,2,0,0
+-2,-2,-2,2
+3,0,2,-1
+-1,3,2,2
+-1,0,-1,0
+0,2,1,-2
+3,0,0,0";
+
+        private const string Input3 = @"
+1,-1,0,1
+2,0,-1,0
+3,2,-1,0
+0,0,3,1
+0,0,-1,-1
+2,3,-2,0
+-2,2,0,0
+2,-2,0,-1
+1,-1,0,-1
+3,2,0,2";
+
+        private const string Input4 = @"
+1,-1,-1,-2
+-2,-2,0,1
+0,2,1,3
+-2,3,-2,1
+0,2,3,-2
+-1,-1,1,-2
+0,-2,-1,0
+-2,2,3,-1
+1,2,2,0
+-1,-2,0,-2";
+
+        [Theory]
+        [InlineData(Input1, 2)]
+        [InlineData(Input2, 4)]
+        [InlineData(Input3, 3)]
+        [InlineData(Input4, 8)]
+        public void Test(string input, int expected)
+        {
+            var result = Day25.Solve(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Solve()
+        {
+            var input = File.ReadAllText("C:\\Code\\aoc2018\\aoc2018\\Data\\input25.txt");
+            var result = Day25.Solve(input);
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
