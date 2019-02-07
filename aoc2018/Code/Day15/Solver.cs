2979d9d92063aca138aa55d7e633ad9900f050f3
Helen Toomik <helen.toomik@tretton37.com>
2019-02-07 22:46:10 +0100

    Day 15: refactor

---
+++ aoc2018/Code/Day15/Solver.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace aoc2018.Code.Day15
+{
+    class Solver
+    {
+        public int Solve2(List<string> map, string print = null)
+        {
+            var elfPower = Engine.DefaultAttackPower;
+            while (true)
+            {
+                var engine = new Engine();
+                engine.Initialize(map, elfPower);
+
+                var startingElfCount = engine.Units.Count(u => u.Race == Race.Elf);
+
+                engine.RunGame();
+
+                if (print != null)
+                {
+                    PrintFinalState(print, engine);
+                }
+
+                var elvesWon = engine.Units[0].Race == Race.Elf;
+                if (elvesWon)
+                {
+                    var elfCount = engine.Units.Count(u => u.Race == Race.Elf);
+                    var elvesLost = startingElfCount - elfCount;
+                    if (elvesLost <= 0)
+                    {
+                        return GetFinalOutcome(engine);
+                    }
+                }
+
+                elfPower++;
+            }
+        }
+        
+        public int Solve1(List<string> map, string print = null)
+        {
+            var engine = new Engine();
+            engine.Initialize(map);
+            engine.RunGame();
+
+            if (print != null)
+            {
+                PrintFinalState(print, engine);
+            }
+
+            var outcome = GetFinalOutcome(engine);
+
+            return outcome;
+        }
+
+        private static void PrintFinalState(string print, Engine engine)
+        {
+            var final = new StringBuilder();
+            final.AppendLine("Ended after " + engine.Rounds);
+            final.AppendLine("Health left " + engine.Units.Sum(u => u.HitPoints));
+            final.AppendLine(GetFinalOutcome(engine).ToString());
+            File.AppendAllText(print, final.ToString());
+        }
+
+        private static int GetFinalOutcome(Engine engine)
+        {
+            var rounds = engine.Rounds;
+            var hitPointsLeft = engine.Units.Sum(u => u.HitPoints);
+            var outcome = rounds * hitPointsLeft;
+            return outcome;
+        }
+    }
+}
--- aoc2018/Test/Test15/Solution.cs
+++ aoc2018/Test/Test15/Solution.cs
@@ -19,14 +19,7 @@ namespace aoc2018.Test.Test15
         public void Solve1()
         {
             var map = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input15.txt").ToList();
-            var engine = new Engine();
-            engine.Initialize(map);
-            engine.RunGame();
-
-            var rounds = engine.Rounds;
-            var hitPointsLeft = engine.Units.Sum(u => u.HitPoints);
-
-            var outcome = rounds * hitPointsLeft;
+            var outcome = new Solver().Solve1(map);
 
             _output.WriteLine(outcome.ToString());
         }
@@ -35,46 +28,8 @@ namespace aoc2018.Test.Test15
         public void Solve2()
         {
             var map = File.ReadAllLines("C:\\Code\\aoc2018\\aoc2018\\Data\\input15.txt").ToList();
-            const string outFile = "C:\\Code\\aoc2018\\output15_1.txt";
-            var elfPower = Engine.DefaultAttackPower + 14;
-            while (true)
-            {
-                Output(outFile, "Trying elf power " + elfPower);
-                var engine = new Engine();
-                engine.Initialize(map, elfPower);
-
-                var startingElfCount = engine.Units.Count(u => u.Race == Race.Elf);
-
-                engine.RunGame();
-
-                var elvesWon = engine.Units[0].Race == Race.Elf;
-                if (!elvesWon)
-                {
-                    Output(outFile, "Elves lost");
-                }
-                else
-                {
-                    var elfCount = engine.Units.Count(u => u.Race == Race.Elf);
-                    var elvesLost = startingElfCount - elfCount;
-                    if (elvesLost > 0)
-                    {
-                        Output(outFile, $"Elves won, but {elvesLost} elves died.");
-                    }
-                    else
-                    {
-                        Output(outFile, "Elves won, no elves died.");
-                        break;
-                    }
-                }
-
-                elfPower++;
-            }
-        }
-
-        private void Output(string path, string message)
-        {
-            _output.WriteLine(message);
-            File.AppendAllText(path, message + "\r\n");
+            var solution = new Solver().Solve2(map);
+            _output.WriteLine(solution.ToString());
         }
     }
 }
